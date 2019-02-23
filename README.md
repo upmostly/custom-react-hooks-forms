f373342f3956e0f2367855d278308d63a8f10e46
James <crowson.j@gmail.com>
2019-02-23 12:49:29 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,6 @@
+
+## Custom React Hooks Form Handler Tutorial
+
+The full tutorial for building this project can be found over at Upmostly.com: [Using Custom React Hooks to Simplify Forms](https://upmostly.com/tutorials/using-react-hooks-simplify-forms).
+
+![A login form built in React and using custom React Hooks to power the form](https://upmostly.com/wp-content/uploads/react-hooks-forms-final.gif)
---
+++ src/Form.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import useForm from "./useForm";
+
+const Form = () => {
+  const { values, handleChange, handleSubmit } = useForm(login);
+
+  function login() {
+    console.log(values);
+  }
+
+  return (
+    <div className="section is-fullheight">
+      <div className="container">
+        <div className="column is-4 is-offset-4">
+          <div className="box">
+            <form onSubmit={handleSubmit}>
+              <div className="field">
+                <label className="label">Email Address</label>
+                <div className="control">
+                  <input className="input" type="email" name="email" onChange={handleChange} value={values.email} required />
+                </div>
+              </div>
+              <div className="field">
+                <label className="label">Password</label>
+                <div className="control">
+                  <input className="input" type="password" name="password" onChange={handleChange} value={values.password} required />
+                </div>
+              </div>
+              <button type="submit" className="button is-block is-info is-fullwidth">Login</button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Form;
